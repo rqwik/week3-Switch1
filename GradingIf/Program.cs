ae9a3add0c66d34e529ebf839e311758e1432abe
marinakamardina <kamardina.mari@gmail.com>
2022-04-06 13:59:09 +0300

    add files

---
+++ GradingIf/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your exam result:");
+            char userInput = Convert.ToChar(Console.ReadLine().ToUpper()); //char - контейнер для одной буквы toupper или tolower перевод
+                                                                           //букв в верхний или нижний регистр
+
+            //A - Outstanding B - Superior C - Good D - Satisfactory E - Low Pass F - Failure 
+
+
+            if (userInput == 'A') //тут обязательно в одни ковычки ' '
+            {
+                Console.WriteLine("Outstanding");
+            }
+            else if (userInput == 'B')
+            {
+                Console.WriteLine("Superior");
+            }
+            else if (userInput == 'C')
+            {
+                Console.WriteLine("Good");
+            }
+            else if (userInput == 'D')
+            {
+                Console.WriteLine("Satisfactory");
+            }
+            else if (userInput == 'E')
+            {
+                Console.WriteLine("Low Pass");
+            }
+            else if (userInput == 'F')
+            {
+                Console.WriteLine("Failure");
+            }
+            else
+            {
+                Console.WriteLine("This isnt an exam result");
+            }
+
+            Console.WriteLine("Have a nice day.");
+        }
+    }
+}
---
+++ GradingSwitch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your exam result:");
+            char userInput = Convert.ToChar(Console.ReadLine().ToUpper());
+            switch (userInput)
+            {
+                case 'A':
+                    Console.WriteLine("Outstanding");
+                    break; // это слово позволяет выйти из блока
+                case 'B':
+                    Console.WriteLine("Superior");
+                    break;
+                case 'C':
+                    Console.WriteLine("Good");
+                    break;
+                case 'D':
+                    Console.WriteLine("Satisfactory");
+                    break;
+                case 'E':
+                    Console.WriteLine("Low Pass");
+                    break;
+                case 'F':
+                    Console.WriteLine("Failure");
+                    break;
+                default:
+                    Console.WriteLine("This isnt an exam result");
+                    break;
+            }
+
+            Console.WriteLine("Have a nice day.");
+        }
+    }
+}
+//A - Outstanding B - Superior C - Good D - Satisfactory E - Low Pass F - Failure 
+
